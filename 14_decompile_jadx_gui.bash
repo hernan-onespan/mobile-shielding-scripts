9709f6e260f943dbfe7b9ff12e09dced3aa403bf
Hernan Giraudo <girauhe1@corp.onespan.com>
2025-06-25 16:18:04 -0300

    backupBeforeMerge

---
+++ 14_decompile_jadx_gui.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source utils/utils.bash
+
+# ================== VALIDAR DATOS DE ENTRADA ==================
+
+if [ -z "$1" ]; then
+  echo "Uso: $0 <apk_dir>"
+  exit 1
+  fi
+  
+apk_dir="${1%/}" # Eliminar barra final si existe
+
+if [ ! -d "$apk_dir" ]; then
+  echo "El directorio '$apk_dir' no existe."
+  exit 1
+  fi
+
+
+# ================== LÓGICA PRINCIPAL ==================
+
+listar_apks $apk_dir
+
+jadx_gui $apk_dir
+
+exit 0
+
+
+
+
